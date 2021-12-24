a0b38a3efea2030214580fb1e15fa3abe492f838
Mark Ghebrial <mghebrialjr@icloud.com>
2021-12-24 14:03:49 -0800

    Read stdout from a minecraft server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::{Command, Stdio};
+use std::io::{BufRead, BufReader, Error, ErrorKind, Write};
+
+fn main() -> Result<(), Error> {
+    //let command = Command::new("python3").arg("testproc.py");
+    let command = Command::new("java").arg("-Xmx1024M").arg("-Xms1024M").arg("-jar").arg("server.jar").arg("nogui")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()?;
+
+    let mut stdin = command.stdin.unwrap();
+    let stdout = command.stdout.unwrap();
+
+    let reader = BufReader::new(stdout);
+
+    reader
+        .lines()
+        .filter_map(|line| line.ok())
+        .for_each(|line| println!("Recieved: {}", line));
+
+    Ok(())
 }
